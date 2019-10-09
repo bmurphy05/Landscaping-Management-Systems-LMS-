9accf8094b52557b2e69e70767cf7b21ec43ba9b
bmurphy05 <bmurphy05@manhattan.edu>
2019-10-09 18:07:06 -0400

    add jest test for register.  modularize start-server

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,4 @@
 import "reflect-metadata";
+import { startServer } from "./startServer";
 
-import { GraphQLServer } from 'graphql-yoga'
-
-
-const typeDefs = `
-  type Query {
-    hello(name: String): String!
-  }
-`
-
-const resolvers = {
-  Query: {
-    hello: (_: any, { name }: any) => `Hello ${name || 'World'}`,
-  },
-}
-
-const server = new GraphQLServer({ typeDefs, resolvers })
-server.start(() => console.log('Server is running on localhost:4000'))
+startServer();
